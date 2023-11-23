83a4ca1a1a73af8c08e10a934628a9b1f5ccb3c7
SamuelCosta <118944536+SamuelCostaDev@users.noreply.github.com>
2023-11-23 08:51:16 -0300

    Publishing my API

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('A variável de ambiente MONGODB_URI não está definida.');
+    }
+  
+    mongoose.connect(process.env.MONGODB_URI)
+      .then(() => {
+        console.log('Conectado ao MongoDB');
+      })
+      .catch((err) => {
+        console.error('Erro na conexão com o MongoDB:', err.message);
+      });
+  } catch (err) {
+    console.error('Erro ao configurar a conexão com o MongoDB:', err.message);
+  }
+  
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
