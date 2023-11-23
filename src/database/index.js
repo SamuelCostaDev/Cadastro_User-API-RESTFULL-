5f7c906b93c6ffdd27bd22dde10d0f4ac60e446a
SamuelCosta <118944536+SamuelCostaDev@users.noreply.github.com>
2023-11-23 08:22:57 -0300

    Publishing my API

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://devsamuelcosta:tiSF0NdEvEfCxMnE@cluster0.rislswl.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URL, {
+    useUnifiendTopology: true,
+    useNewUrlParser: true,
+},
+() => {
+    console.log(("DB connected"))
+}
+);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
